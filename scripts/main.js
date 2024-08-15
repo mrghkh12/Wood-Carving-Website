2f163bcfb86fc139397906f2fcc705989ca24865
mohammad reza <mrghkh12@gmail.com>
2024-08-15 21:00:35 +0330

    feat: responsive for small screen siza

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,5 @@ const headerNavbar = document.querySelector('.header-section')
 window.addEventListener('scroll' , () => {
     console.log(window.scrollY);
     
-    headerNavbar.classList.toggle ('sticky', window.scrollY > 150)
+    headerNavbar.classList.toggle ('sticky', window.scrollY > 70)
 })
