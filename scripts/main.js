b5c971b6ae1c92d45544ba6d355c4230eca3b156
mohammad reza <mrghkh12@gmail.com>
2024-08-15 21:08:22 +0330

    feat(main,js): add event for opne and close header navbar menu

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,16 @@
 
 const headerNavbar = document.querySelector('.header-section')
 
+let menuBtn = document.querySelector('#menuBtn')
+let navMenuLink = document.querySelector('.navMenuLink')
+
+menuBtn.addEventListener('click' , () =>{
+    menuBtn.classList.toggle('bx-x')
+    navMenuLink.classList.toggle('open')
+})
+
 window.addEventListener('scroll' , () => {
-    console.log(window.scrollY);
-    
     headerNavbar.classList.toggle ('sticky', window.scrollY > 70)
+    menuBtn.classList.remove('bx-x')
+    navMenuLink.classList.remove('open')
 })
